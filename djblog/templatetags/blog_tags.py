6f119767ccd8f2c86917584acfda31ad3f759c66
huangxiang <huangxiang1992@hotmail.com>
2019-07-19 17:19:44 +0800

    1. 添加自定义分类模版标签。 使用该方式替代内置简单分页

--- djblog/templatetags/blog_tags.py
+++ djblog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from ..models import Post, CateGory
+from django.core.paginator import EmptyPage
 
 register = template.Library()
 
@@ -17,3 +18,47 @@ def archives():
 @register.simple_tag
 def get_categories():
     return CateGory.objects.all()
+
+
+@register.inclusion_tag("paginator.html", takes_context=True)
+def render_paginator(context, adjacent_pages=3):
+    start_page = max(context["page_obj"].number - adjacent_pages, 1)
+    if start_page <= 3:
+        start_page = 1
+    end_page = context["page_obj"].number + adjacent_pages + 1
+    if end_page >= context["paginator"].num_pages - 1:
+        end_page = context["paginator"].num_pages + 1
+
+    page_numbers = [
+        n
+        for n in range(start_page, end_page)
+        if n in range(1, context["paginator"].num_pages + 1)
+    ]
+
+    page_obj = context["page_obj"]
+    paginator = context["paginator"]
+
+    try:
+        next_ = context["page_obj"].next_page_number()
+    except EmptyPage:
+        next_ = None
+
+    try:
+        previous = context["page_obj"].previous_page_number()
+    except EmptyPage:
+        previous = None
+
+    return {
+        "page_obj": page_obj,
+        "paginator": paginator,
+        "page": context["page_obj"].number,
+        "pages": context["paginator"].num_pages,
+        "page_numbers": page_numbers,
+        "next": next_,
+        "previous": previous,
+        "has_next": context["page_obj"].has_next(),
+        "has_previous": context["page_obj"].has_previous(),
+        "show_first": 1 not in page_numbers,
+        "show_last": context["paginator"].num_pages not in page_numbers,
+        "request": context["request"],
+    }
--- djblog/views.py
+++ djblog/views.py
@@ -15,7 +15,82 @@ class IndexView(ListView):
     model = Post  # 告诉Django获取的模型
     template_name = "index.html"  # 指定这个视图渲染的模版
     context_object_name = "post_list"  # 指定获取的模型列表数据保存的变量名，这个变量会传递到模版
-    paginate_by = 10
+    paginate_by = 1
+
+    # def pagination_data(self, paginator, page, is_paginated):
+    #     if not is_paginated:
+    #         return {}
+
+    #     left = []
+    #     right = []
+
+    #     left_has_more = False
+    #     right_has_more = False
+
+    #     first = False
+    #     last = False
+    #     page_number = page.number
+
+    #     total_pages = paginator.num_pages
+    #     page_range = paginator.page_range
+
+    #     if page_number == 1:
+    #         right = page_range[page_number:page_number + 2]
+
+    #         if right[-1] < total_pages - 1:
+    #             right_has_more = True
+
+    #         if right[-1] < total_pages:
+    #             last = True
+
+    #     elif page_number == total_pages:
+    #         left = page_range[
+    #             (page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1
+    #         ]
+
+    #         if left[0] > 2:
+    #             left_has_more = True
+
+    #         if left[0] > 1:
+    #             first = True
+
+    #     else:
+    #         left = page_range[
+    #             (page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1
+    #         ]
+    #         right = page_range[page_number:page_number + 2]
+
+    #         if right[-1] < total_pages - 1:
+    #             right_has_more = True
+    #         if right[-1] < total_pages:
+    #             last = True
+
+    #         if left[0] > 2:
+    #             left_has_more = True
+    #         if left[0] > 1:
+    #             first = True
+
+    #     data = {
+    #         "left": left,
+    #         "right": right,
+    #         "left_has_more": left_has_more,
+    #         "right_has_more": right_has_more,
+    #         "first": first,
+    #         "last": last,
+    #     }
+
+    #     return data
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+
+    #     paginator = context.get("paginator")
+
+    #     pagination_data = self.pagination_data(paginator, page, is_paginated)
+
+    #     context.update(pagination_data)
+
+    #     return context
 
 
 # def index(request):
